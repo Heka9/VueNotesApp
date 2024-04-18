0ca19a2a088069b87aa0356cc71b0315474f658e
Yevhen <bliznyukjenia93@gmail.com>
2024-04-18 11:16:21 +0300

    commit 3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   pablicPath: `/VueNotesApp/`,
+   publicPath: `/VueNotesApp/`,
    plugins: [vue()],
    resolve: {
       alias: {
